f5f142a8c91705ee8ae8212b18180ff4e02239c0
Scott G. Daniel <scottdaniel25@gmail.com>
2017-10-17 11:39:20 -0700

    starting on centrifuge script

---
+++ scripts/opt-centrifuge.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#
+# Script to run centrifuge 
+#
+
+set -u
+
+CONFIG="./config.sh"
+
+if [ -e $CONFIG ]; then
+    . "$CONFIG"
+else
+    echo Missing config \"$CONFIG\" ermagod!
+    exit 12345
+fi
+
+export CWD="$PWD"
+export STEP_SIZE=1
+
+PROG=`basename $0 ".sh"`
+STDOUT_DIR="$CWD/out/$PROG"
+
+init_dir "$STDOUT_DIR" 
+
+cd $PRJ_DIR
+
+export LIST="fna_list"
+
+find $DNA_DIR -iname "*.fastq" > $DNALIST
+
+export TODO="files_todo"
+
+if [ -e $TODO ]; then
+    rm $TODO
+fi
+
+for FASTA in $(cat $LIST); do
+
+    if [ ! -e ""$FASTA".rev.2.bt2" ]; then
+        echo $FASTA >> $TODO
+    fi
+
+done
+
+NUM_FILES=$(lc $TODO)
+
+echo Found \"$NUM_FILES\" files in \"$BT2_DIR\" to work on
+
+JOB=$(qsub -J 1-$NUM_FILES:$STEP_SIZE -V -N centrifuge -j oe -o "$STDOUT_DIR" $WORKER_DIR/runCentrifuge.sh)
+
+if [ $? -eq 0 ]; then
+  echo -e "Submitted job \"$JOB\" for you in steps of \"$STEP_SIZE.\"
+  There's no such thing as bad language. I don't believe that any more. That's
+  ridiculous. They call it a "debasing of the language?" No! We are adults.
+  These are the words that WE use, to express frustration, rage, anger―in
+  order that we don't pick up a tire iron and beat the shit out of someone.
+          ― Lewis Black"
+else
+  echo -e "\nError submitting job\n$JOB\n"
+fi
+echo done $(date)
+
