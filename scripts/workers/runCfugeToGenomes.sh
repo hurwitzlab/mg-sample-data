863bfca81bd74c000476af709a866e77f544e23c
Scott G. Daniel <scottdaniel25@gmail.com>
2017-12-21 17:38:47 -0700

    this is where it stops for the night

--- scripts/workers/runCfugeToGenomes.sh
+++ scripts/workers/runCfugeToGenomes.sh
@@ -9,6 +9,7 @@
 #PBS -M scottdaniel@email.arizona.edu
 #PBS -m bea
 
+module load singularity
 unset module
 set -u
 
@@ -29,15 +30,63 @@ mkdir -p $GENOME_DIR
 
 cd $GENOME_DIR
 
+#for REPORT in $(cat $TODO); do
+#
+#    echo Working on report $REPORT
+#
+#    python $WORKER_DIR/cfuge_report_to_genome.py -r $REPORT
+#    
+#    echo Unzipping gotten fastas and gffs
+#
+#    find ./ -iname "*.gz" | xargs -I file gunzip file
+#
+#done
+
+#Not using the python script anymore
+#Patric apparently has better function annotation of the refseq gffs!
+
+export p3="singularity exec $SING_IMG/p3-tools.img"
+
 for REPORT in $(cat $TODO); do
 
     echo Working on report $REPORT
 
-    python $WORKER_DIR/cfuge_report_to_genome.py -r $REPORT
-    
-    echo Unzipping gotten fastas and gffs
+    mkdir $(basename $REPORT)
+    cd $(basename $REPORT)
 
-    find ./ -iname "*.gz" | xargs -I file gunzip file
+    TEMPFILE=$(mktemp)
+
+    python $WORKER_DIR/cfuge_report_filter.py -r $REPORT > $TEMPFILE
+
+    while read LINE; do
+       
+        taxon_id=$(echo $LINE | cut -f 1 -d ';')
+        name=$(echo $LINE | cut -f 2 -d ';')
+
+        echo "Taxon ID is $taxon_id"
+        echo "Species is $name"
+
+#not using -e species,"$name" 
+        while read patricID; do 
+
+            echo "Getting PATRIC genome_id $patricID in fasta and Refseq gff formats"
+
+            wget -nc -nd -r --no-parent -A '*.fna' \
+                ftp://ftp.patricbrc.org/patric2/patric3/genomes/"$patricID"
+            
+            wget -nc -nd -r --no-parent -A '*.RefSeq.gff' \
+                ftp://ftp.patricbrc.org/patric2/patric3/genomes/"$patricID"
+
+            if [[ ! -e "$patricID".RefSeq.gff ]]; then
+                
+                wget -nc -nd -r --no-parent -A '*.PATRIC.gff' \
+                    ftp://ftp.patricbrc.org/patric2/patric3/genomes/"$patricID"
+
+            fi
+
+        done < $($p3 p3-all-genomes -e taxon_id,"$taxon_id" -e genome_status,"Complete" | egrep -v "genome")
+
+    done < $TEMPFILE
 
 done
 
